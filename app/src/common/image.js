e17d077bba4437d1902fa8b9da0ab6e208879c26
Jarle Trollebø <aborysa@gmail.com>
2017-01-25 21:57:19 +0100

    Fixed typo

--- app/src/common/image.js
+++ app/src/common/image.js
@@ -1,4 +1,4 @@
-
+import { BASE } from './constants';
 
 export const jsonToImage = (json) => {
   return (
@@ -18,19 +18,22 @@ export const jsonToImage = (json) => {
   );
 }
 
-
 export class Image{
   constructor(id,name,description,thumb,original,wide,lg,md,sm,xs,tags,photographer){
     this._id = id;
     this._name = name;
     this._description = description;
-    this._thumb = thumn;
+    this._thumb = thumb;
     this._original = original;
     this._wide = wide;
     this._lg = lg;
+    this._md = md;
+    this._xs = xs;
+    this._tags = tags;
+    this._photographer = photographer;
   }
-  getThumb(){
-    return this.thumb;
+  get thumb(){
+    return `${BASE}${this._thumb}`;
   }
 
 }
