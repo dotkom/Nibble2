1422217b906419bdf3ad6c44aa5ba0be7af85770
Jarle Trollebø <aborysa@gmail.com>
2017-03-01 21:35:07 +0100

    Example constants

---
+++ app/src/common/constants.example.js
@@ -0,0 +1,12 @@
+export const BASE = 'http://localhost/';
+export const API_BASE = `${BASE}api/v1/`;
+export const API_EVENTS = 'events/';
+export const API_AUTH = 'auth/';
+export const API_ATTENDEES = 'attendees/';
+export const API_ATTEND = 'attend/';
+export const API_USERS = 'usersaldo/';
+export const API_INVENTORY = 'inventory/';
+export const API_RFID = "rfid/";
+
+export const CLIENT_SECRET = '';
+export const CLIENT_ID = '';
