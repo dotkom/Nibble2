3f1924c630a62d9d9072fd3b5d23831531aeb853
Jarle Trollebø <aborysa@gmail.com>
2017-05-03 17:36:04 +0200

    Added css + js for keyboard and started working on a Keyboard component

---
+++ app/src/components/Keyboard.jsx
@@ -0,0 +1,28 @@
+
+
+
+
+import React, {Component} from 'react';
+
+
+export class Keyboard extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  componentDidMount(){
+  
+  }
+
+  componentWillUnmount(){
+    
+  }
+
+  onentWillReceiveProps(props){
+    
+  }
+
+  render(){
+    return <div>{this.props.children}</div>
+  }
+}
