4835efc56d258321bfb9b792f0e846f27bcbf259
Jarle Trollebø <aborysa@gmail.com>
2017-05-03 22:58:01 +0200

    Redirect issues to nibble2 repo

--- app/src/components/modals.jsx
+++ app/src/components/modals.jsx
@@ -42,12 +42,12 @@ export const HelpModal = props => (<Modal
   <div>
     <b>Hvordan legger jeg inn penger?</b>
     <p>
-        Du kan legge inn penger manuelt på appen etter du har logget inn eller du kan legge til med ditt kredittkort på online.ntnu.no under min profil.
-      </p>
+      Du kan legge inn penger manuelt på appen etter du har logget inn eller du kan legge til med ditt kredittkort på online.ntnu.no under min profil.
+    </p>
     <b>Det er tomt for en vare, hva gjør jeg?</b>
     <p>Det er funksjonalitet for automatisk varsling under utvikling men foreløpig må du sende mail til trikom@online.ntnu.no.</p>
     <b>Jeg fant en feil, hva gjør jeg?</b>
-    <p>Legg til en issue på github.com/dotKom/nibble/ eller send en mail til dotkom@online.ntnu.no</p>
+    <p>Legg til en issue på github.com/dotKom/nibble2/ eller send en mail til dotkom@online.ntnu.no</p>
   </div>
 </Modal>);
 
