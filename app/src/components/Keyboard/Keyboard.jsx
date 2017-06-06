ed0f57c16ed51b977a4cf69a64889fd7b8a87378
Jarle Trollebø <aborysa@gmail.com>
2017-06-06 05:51:20 +0200

    Made the Keyboard component more extensible now it function 100% as a proper wrapper for virtual-keyboard

--- app/src/components/Keyboard/Keyboard.jsx
+++ app/src/components/Keyboard/Keyboard.jsx
@@ -1,11 +1,12 @@
+import React from 'react';
 
-import React, {Component} from 'react';
-
+import _ from 'lodash';
 
 
 export class Keyboard extends React.Component{
   constructor(props){
     super(props);
+    this.child = null;
   }
   
   handleChange(evt,keyboard,elm){
@@ -13,16 +14,25 @@ export class Keyboard extends React.Component{
       this.props.onChange(elm.value);
     }
   }
-
+  componentDidMount(){
+    if(this.child)
+      $(this.child).keyboard(
+        _.assign(
+          //Defaults
+          {
+            usePreview: false,
+            autoAccept: true,
+            layout: this.props.layout || 'qwerty-no',
+            accepted: (...a) => this.handleChange(...a),
+            canceled: (...a) => this.handleChange(...a) 
+          },
+          //Props
+          _.omit(this.props,['onChange','children','accepted','canceled'])
+        )
+      );
+  }
   childRef(element){
-    //Default element
-    $(element).keyboard({
-      accepted: (...a) => this.handleChange(...a),
-      canceled: (...a) => this.handleChange(...a),
-      usePreview: false,
-      autoAccept: true,
-      layout: this.props.layout || 'qwerty-no' 
-    });
+    this.child = element;
   }
 
   render(){
